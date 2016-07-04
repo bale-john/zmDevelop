97973be8c6837ba679e0ece3c378e0291ba89358
zhangguowei <zhangguowei@pku.edu.cn>
2016-07-04 19:32:32 +0800

    fix bugs
    Signed-off-by: zhangguowei <zhangguowei@pku.edu.cn>

--- ServiceItem.cpp
+++ ServiceItem.cpp
@@ -6,27 +6,28 @@
 //support in_addr. It's a ip struct
 #include <netinet/in.h>
 #include "ConstDef.h"
+#include <cstring>
 using namespace std;
 
 ServiceItem::ServiceItem():
-    _serviceFather(""),
     _host(""),
     _port(-1),
     _connRetry(0),
     _connTimeout(3),// default 3 seconds
+    _serviceFather(""),
     _status(STATUS_DOWN) 
 {   
     memset(&_addr, 0, sizeof(struct in_addr));
 }
 
 ServiceItem::ServiceItem(std::string host, struct in_addr *addr, int port, int connRetry, int timeout, std::string serviceFather, int status):
-	_serviceFather(serviceFather),
 	_host(host),
+	_addr(*addr),
 	_port(port),
 	_connRetry(connRetry),
 	_connTimeout(timeout),
-	_status(status),
-	_addr(*addr){
+	_serviceFather(serviceFather),
+	_status(status){
 
 	}
 
