1cc216c9040f45bf32caee930d081902e95c6528
bale-john <zhangguowei@pku.edu.cn>
2016-07-11 19:23:46 +0800

    fix things in class Config
    Signed-off-by: bale-john <zhangguowei@pku.edu.cn>

--- Config.h
+++ Config.h
@@ -9,36 +9,6 @@
 using namespace std;
 
 class Config {
-public:
-	static Config* getInstance();
-	int load();
-	int resetConfig();
-	int isDaemonMode();
-	string getMonitorHostname();
-	int isAutoStart();
-	int getLogLevel();
-	int getConnRetryCount();
-	int getScanInterval();
-	string getInstanceName();
-	string getZkHost();
-	string getZkLogPath();
-	void clearServiceMap();
-	int getZkRecvTimeout();
-	//todo maybe should return const string
-	string getNodeList();
-	string getMonitorList();
-	//这个名字不应该叫add，叫set
-	int addService(string ipPath, ServiceItem serviceItem);
-	//todo 这个print在Util类中怎么看都应该更好，现在将就一下
-	int printMap();
-	map<string, ServiceItem> getServiceMap();
-	int setServiceFatherToIp(unordered_map<string, unordered_set<string>> sft);
-	unordered_map<string, unordered_set<string>>& getServiceFatherToIp();
-	//用来保存每个serviceFather拥有各种不同类型的节点的数目
-	unordered_map<string, vector<int>> serviceFatherStatus;
-	int setServiceMap(string node, int val);
-
-
 private:
 	Config();
 	~Config();
@@ -54,9 +24,42 @@ private:
 	std::string _zkLogPath;
 	map<string, ServiceItem> _serviceMap;
 	int _zkRecvTimeout;
+	unordered_map<string, unordered_set<string>> serviceFatherToIp;
 	int setValueInt(const string& key, const string& value);
 	int setValueStr(const string& key, const string& value);
-	unordered_map<string, unordered_set<string>> serviceFatherToIp;
 
+public:
+	static Config* getInstance();
+	int load();
+	int resetConfig();
+	int isDaemonMode();
+	int isAutoStart();
+
+	string getMonitorHostname();
+	int getLogLevel();
+	int getConnRetryCount();
+	int getScanInterval();
+	string getInstanceName();
+	string getZkHost();
+	string getZkLogPath();
+	int getZkRecvTimeout();
+	//todo maybe should return const string
+	string getNodeList();
+	string getMonitorList();
+
+	map<string, ServiceItem> getServiceMap();
+	int setServiceMap(string node, int val);
+
+	unordered_map<string, unordered_set<string>>& getServiceFatherToIp();
+	int setServiceFatherToIp(unordered_map<string, unordered_set<string>> sft);
+
+	//这个名字不应该叫add，叫set，方便其他对象调用
+	int addService(string ipPath, ServiceItem serviceItem);
+	//put this method to class Util may be better
+	int printMap();
+	
+	//用来保存每个serviceFather拥有各种不同类型的节点的数目
+	unordered_map<string, vector<int>> serviceFatherStatus;
+	void clearServiceMap();
 };
 #endif
