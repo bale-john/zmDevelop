c17a7e627b5555620d0e6b562991e43265740d83
bale-john <zhangguowei@pku.edu.cn>
2016-07-14 16:38:20 +0800

    adapt code in class ServiceListener.h
    Signed-off-by: bale-john <zhangguowei@pku.edu.cn>

--- ServiceListener.h
+++ ServiceListener.h
@@ -19,38 +19,51 @@ class ServiceListener {
 //private:
 public:
 	ServiceListener();
+	static ServiceListener* slInstance;
 	zhandle_t* zh;
-	//其中的ip是单纯的ip，没有包含前缀路径，这个结构体可以认为是这个类的核心，不应该放一份拷贝到Config里面
+	//key is serviceFather and value is ipPort
 	unordered_map<string, unordered_set<string>> serviceFatherToIp;
+	/*
+	key is service father and value is the number of ipPort with different types.
+	It's used for check weather there are only one service alive 
+	*/
+	unordered_map<string, vector<int>> serviceFatherStatus;
+
+	Config* conf;
+	LoadBalance* lb;
 	//there exist some common method. I should make a base class maybe
 	int initEnv();
 	int destroyEnv();
-	Config* conf;
     int zkGetChildren(const string path, struct String_vector* children);
-    static ServiceListener* slInstance;
-    LoadBalance* lb;
     size_t getIpNum(const string& serviceFather);
-    //用来保存每个serviceFather拥有各种不同类型的节点的数目
-	unordered_map<string, vector<int>> serviceFatherStatus;
+
 
 public:
 	static ServiceListener* getInstance();
 	~ServiceListener();
-	int addChildren(const string serviceFather, struct String_vector children);
+
 	int getAllIp();
+
 	int loadService(string path, string serviceFather, string ipPort, vector<int>& );
 	int loadAllService();
+
     int zkGetNode(const char* path, char* data, int* dataLen);
+    int addChildren(const string serviceFather, struct String_vector children);
+
     int getAddrByHost(const char* host, struct in_addr* addr);
+
     void modifyServiceFatherToIp(const string op, const string& path);
+    unordered_map<string, unordered_set<string>>& getServiceFatherToIp();
+
     static void watcher(zhandle_t* zhandle, int type, int state, const char* node, void* context);
     static void processDeleteEvent(zhandle_t* zhandle, const string& path);
     static void processChildEvent(zhandle_t* zhandle, const string& path);
     static void processChangedEvent(zhandle_t* zhandle, const string& path);
+
     size_t getServiceFatherNum();
+
     int modifyServiceFatherStatus(const string& serviceFather, int status, int op);
 	int modifyServiceFatherStatus(const string& serviceFather, vector<int>& statusv);
 	int getServiceFatherStatus(const string& serviceFather, int status);
-	unordered_map<string, unordered_set<string>>& getServiceFatherToIp();
 };
 #endif
